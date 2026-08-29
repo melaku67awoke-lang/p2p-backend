ae9822c63bd855b94f3796ce2aff14dee9b2506e
melaku67awoke-lang <melaku67awoke@gmail.com>
2026-08-29 20:55:38 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+
+// Connect to MongoDB Atlas
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log('Connected to MongoDB Atlas successfully!'))
+  .catch((err) => console.error('MongoDB connection error:', err));
 
 app.get('/', (req, res) => {
   res.send('P2P Backend is running successfully! 🚀');
