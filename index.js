727952cf1c371210fdc491aac62d88ec66639789
melaku67awoke-lang <melaku67awoke@gmail.com>
2026-08-29 20:53:20 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,18 +1,11 @@
 const express = require('express');
-const mongoose = require('mongoose');
-const cors = require('cors');
-
 const app = express();
-app.use(express.json());
-app.use(cors());
-
-const MONGO_URI = "mongodb+srv://melaku67awoke_db_user:55vER2qx6x1NlqHn@cluster0.jphkwdb.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
-
-mongoose.connect(MONGO_URI)
-  .then(() => console.log("Connected to MongoDB Atlas successfully!"))
-  .catch((err) => console.error("MongoDB connection error:", err));
-
 const PORT = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+  res.send('P2P Backend is running successfully! 🚀');
+});
+
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
